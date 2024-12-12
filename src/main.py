54000958907fe1bb332dbff961c3b8a8c8f98d8f
GavinTC <gtclonts@gmail.com>
2024-12-12 15:53:30 -0500

    Added playlist, adjusted play() command, added skip, playlist, add, clear, stop, commands, added several helper functions

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import json
+from bs4 import BeautifulSoup
+import requests
 import discord
 from discord import FFmpegPCMAudio
 from discord.ext import commands
@@ -32,6 +34,16 @@ command_list = [
     "volume",
     "help"
 ]
+
+# guild_id: ["link"]
+playlist_dict: dict[int, list[str]] = {
+}
+
+# guild_id: song_index
+playlist_index: dict[int, int] = {
+    
+}
+
 TOKEN = open("private/token.txt", "r").read()
 intents.message_content = True
 bot = commands.Bot(command_prefix=command_prefix, intents=intents)
@@ -59,7 +71,7 @@ async def on_message(message):
         if command == "play":
             if not arguments:
                 correct_command_usage = False
-                await message.channel.send(f"You must provide a link with the `play` command!")
+                await message.channel.send(f"**You must provide a link with the `play` command!**")
 
     # Don't process commands if they weren't used properly
     if correct_command_usage:
@@ -72,56 +84,145 @@ async def play(ctx: Context, link: str):
     # Join user's voice channel if they're in one
     if ctx.author.voice:
         # Join the voice channel if we're not in it already
-        if not ctx.voice_client: await ctx.author.voice.channel.connect()
+        if not ctx.voice_client:
+            await ctx.author.voice.channel.connect()
+        await ctx.guild.change_voice_state(channel=ctx.voice_client.channel, self_deaf=True)
 
-        # Stop playing audio if it's already playing
+        url = await get_audio_url(link)
+        if url == None:
+            await ctx.send(f"**There was an error fetching the audio!**")
+            return
+
         if ctx.voice_client.is_playing():
-            await ctx.send(f"**Currently playing song!**")
+            await add(ctx, link) # Add the song to the guild's playlist
         else:
-            url = await get_audio_url(link)
-            if url == None:
-                await ctx.send(f"There was an error fetching the audio!")
-                return
-            
-            print(f"url: {url}")
-            
             ctx.voice_client.play(FFmpegPCMAudio(url))
-            await ctx.send(f"***Now playing:***\n{link}")
-        
+            await ctx.send(f"**Now playing:**\n{link}")
     else:
-        await ctx.send("You must be in a voice channel to use that command!")
+        await ctx.send("**You must be in a voice channel to use that command!**")
 
 
-async def get_audio_url(link: str):
-    ydl_opts = {
-        'format': 'bestaudio',
-        'extractaudio': True,
-    }
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info_dict = ydl.extract_info(link, download=False)
-        
-        with open("info_dict_dump.json", "w") as f:
-            json.dump(info_dict, f, indent=5)
 
-        if 'entries' in info_dict:
-            for entry in info_dict['entries']:
-                if entry.get('acodec') not in ['none', None] and 'audio' in entry['format']:
-                    return entry['url']
-        elif 'formats' in info_dict:
-            for format in info_dict['formats']:
-                if format.get('acodec') not in ['none', None] and 'audio' in format['format']:
-                    return format['url']
-    return None
+# Skips the current song, plays the next in queue.
+@bot.command()
+async def skip(ctx: Context):
+    if ctx.voice_client.is_playing():
+        ctx.voice_client.stop()
+
+    await ctx.send("**Skipping current song...**")
+    # If there's a song in the playlist, play it
+    # If not, then basically act as stop()
+    if len(playlist_dict[ctx.guild.id]) == 0:
+        await play(ctx, playlist_dict[ctx.guild.id][0])
+        playlist_dict[ctx.guild.id].pop(0)
+
+    print(f"playlist dict: {playlist_dict[ctx.guild.id]}")
+
+
+# Displays the guild's playlist
+@bot.command()
+async def playlist(ctx: Context):
+    # Create an empty entry if one doesn't exist
+    if ctx.guild.id not in playlist_dict:
+        playlist_dict[ctx.guild.id] = []
+    
+    playlist: list[str] = playlist_dict[ctx.guild.id]
+    playlist_message = ""
+
+    if len(playlist) == 0:
+        await ctx.send(f"**Your playlist is empty!**")
+        return
+    
+    for item in playlist:
+        audio_name = await get_audio_name(item)
+
+        if audio_name != None:
+            print(f"playlist item name: {audio_name}")
+        else:
+            print(f"playlist item: {item}")
+
+        playlist_message += f"\n## **{playlist.index(item) + 1}.** **`{audio_name}`**" #| ({audio_length})"
+    
+    await ctx.send(f"**Playlist:**" + playlist_message)
 
 
 @bot.command()
+async def add(ctx: Context, link: str):
+    await ctx.send(f"**Adding that song to your playlist...**")
+    await add_song_to_playlist(ctx, link)
+
+
+@bot.command()
+async def clear(ctx: Context):
+    playlist_dict[ctx.guild.id] = []
+    await ctx.send(f"**Playlist has been cleared!**")
 
 
 @bot.command()
 async def stop(ctx: Context):
     # Disconnect if the bot is in a voice channel
-    if ctx.voice_client: await ctx.voice_client.disconnect()
-    else:                await ctx.send("I am not currently in a voice channel!")
+    if ctx.voice_client:
+        await ctx.voice_client.disconnect()
+        return
+    await ctx.send("I am not currently in a voice channel!")
         
+
+# Helper methods
+
+async def get_info_dict(link: str):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'extractaudio': True,
+        'extractflat': True,
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        # If the audio doesn't exist it will return an error
+        try:
+            return ydl.extract_info(link, download=False)
+        except:
+            return None
+
+# Attempts to find audio stream url
+async def get_audio_url(link: str):
+    info_dict = await get_info_dict(link)
+
+    if info_dict == None:
+        return None
+        
+    with open("info_dict_dump.json", "w") as f:
+        json.dump(info_dict, f, indent=5)
+
+    if 'entries' in info_dict:
+        for entry in info_dict['entries']:
+            if entry.get('acodec') not in ['none', None] and 'audio' in entry['format']:
+                return entry['url']
+    elif 'formats' in info_dict:
+        for format in info_dict['formats']:
+            if format.get('acodec') not in ['none', None] and 'audio' in format['format']:
+                return format['url']
+    return None
+
+
+# Adds a song to the guild playlist
+async def add_song_to_playlist(ctx: Context, link: str):
+    if ctx.guild.id not in playlist_dict:
+        playlist_dict[ctx.guild.id] = []
+    playlist_dict[ctx.guild.id].append(link)
+
+
+async def get_audio_name(link: str):
+    info_dict = await get_info_dict(link)
+
+    with open("info_dict_dump2.json", "w") as f:
+        json.dump(info_dict, f, indent=5)
+
+    if 'title' in info_dict:
+        return info_dict['title']
+    return None
+
+
+async def get_audio_length(link: str):
+    pass
+
 
 bot.run(token=TOKEN)
